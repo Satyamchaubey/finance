9160005146c2e83e5efb3083ab2a9036b0cbb53a
Satyamchaubey <115795299+Satyamchaubey@users.noreply.github.com>
2024-09-20 15:05:11 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
+// Transaction Schema
 const transactionSchema = new mongoose.Schema({
-    description: String,
-    type: { type: String, enum: ['Expense', 'Income'] },
-    date: Date,
-    amount: Number
+    description: { type: String, required: true },
+    type: { type: String, enum: ['Expense', 'Income'], required: true },
+    date: { type: Date, required: true },
+    amount: { type: Number, required: true }
 });
 
-
+// User Schema
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, unique: true },
@@ -17,11 +18,12 @@ const userSchema = new mongoose.Schema({
         unique: true,
         sparse: true, // Allows multiple null values in unique fields
     },
-    transactions: Array,
+    transactions: [transactionSchema], // Store transactions as an array of transaction documents
     profilePicture: { type: String }, // Add this field
     passwordResetToken: String, // Token for password reset functionality
-    passwordResetExpires: Date // Expiration time for the token
-  });
-  
+    passwordResetExpires: Date, // Expiration time for the token
+    otp: { type: String },          // Field for OTP
+    otpExpiration: { type: Date },  // Expiration time for OTP
+});
 
 module.exports = mongoose.model('User', userSchema);
