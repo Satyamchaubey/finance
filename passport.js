09a072eb3461b69a63791aa27f9ef32a8f1b3990
Satyamchaubey <115795299+Satyamchaubey@users.noreply.github.com>
2024-09-20 09:52:56 +0530

    Add files via upload

---
+++ passport.js
@@ -0,0 +1,60 @@
+const LocalStrategy = require('passport-local').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+module.exports = function(passport) {
+    // Local Strategy (Email & Password)
+    passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+        try {
+            const user = await User.findOne({ email });
+            if (!user) {
+                return done(null, false, { message: 'No user found' });
+            }
+
+            // Match password
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return done(null, false, { message: 'Incorrect password' });
+            }
+
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    }));
+
+    // Google OAuth Strategy
+    passport.use(new GoogleStrategy({
+        clientID: '1042265845512-4hv8t6ot16rcvjvekmgialmrecetmnc0.apps.googleusercontent.com',
+        clientSecret: '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',
+        callbackURL: 'https://saaft.vercel.app//auth/google/callback'
+    },
+    async (accessToken, refreshToken, profile, done) => {
+        try {
+            const existingUser = await User.findOne({ googleId: profile.id });
+            if (existingUser) {
+                return done(null, existingUser);
+            } else {
+                const newUser = new User({
+                    googleId: profile.id,
+                    username: profile.displayName,
+                    email: profile.emails[0].value
+                });
+                await newUser.save();
+                return done(null, newUser);
+            }
+        } catch (err) {
+            return done(err);
+        }
+    }));
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(async (id, done) => {
+        const user = await User.findById(id);
+        done(null, user);
+    });
+};
