dddcd8ace253165808dbecd7cffcb02e0e2906e7
Satyamchaubey <115795299+Satyamchaubey@users.noreply.github.com>
2024-09-20 12:22:23 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use(session({
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback',
+    callbackURL: 'https://saaft.vercel.app/auth/google/callback',
     scope: ['profile', 'email']
 }, async (accessToken, refreshToken, profile, done) => {
     try {
